575bbbc053ddeb7ca3d368c1b1345f4cc92d1595
Faraz Fazli <farazfazli@gmail.com>
2016-11-01 01:10:23 -0400

    Initial commit

---
+++ aarpack.sh
@@ -0,0 +1,27 @@
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+
+echo "${green}Copying files...${magenta}"
+cp **/build/outputs/aar/*.aar .
+echo "Zipping files...${yellow}"
+zip -q pack.zip *.aar
+echo "Uploading...${cyan}"
+# Adapted from mixtape.moe basic uploader, removed jshon dep
+DEST="https://mixtape.moe/upload.php"
+FILE="pack.zip"
+CMD=$(basename "$0")
+OUT=$(curl -sf -F files[]="@${FILE}" "${DEST}")
+if [[ $(echo "$OUT") =~ true ]]; then
+                echo $(echo "$OUT" | python -mjson.tool | grep url | tr -d '"' | tr -d " " | cut -c 5-)
+else
+                echo "An error occurred!"
+                exit 1
+        fi
+
+rm -f pack.zip
+rm -f *.aar
+
+echo "${white}Cleaned up"
